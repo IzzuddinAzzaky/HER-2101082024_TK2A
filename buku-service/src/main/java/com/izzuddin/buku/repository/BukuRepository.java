ef9939297e716b7b92264c824bac452fa69d2154
IzzuddinAzzaky <113503305+IzzuddinAzzaky@users.noreply.github.com>
2023-01-27 23:43:26 +0700

    her
    her

---
+++ buku-service/src/main/java/com/izzuddin/buku/repository/BukuRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.izzuddin.buku.repository;
+
+import com.izzuddin.buku.entity.Buku;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author ASUS
+ */
+public interface BukuRepository extends JpaRepository<Buku, Long>{
+
+    public Buku findByBukuId(Long bukuId);
+    
+}
---
+++ buku-service/src/main/java/com/izzuddin/buku/service/BukuService.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.izzuddin.buku.service;
+
+import com.izzuddin.buku.entity.Buku;
+import com.izzuddin.buku.repository.BukuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author ASUS
+ */
+@Service
+public class BukuService {
+    @Autowired
+    private BukuRepository bukuRepository;
+    
+    public Buku saveBuku(Buku buku){
+        return bukuRepository.save(buku);
+    }
+    
+    public Buku findBukuById(Long bukuId){
+        return bukuRepository.findByBukuId(bukuId);
+    }
+
+
+
+}
